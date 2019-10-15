0f32975efc3b3d67e82f305bffa2bc2be489d5a1
PerthDm(Mac) <prathza55@hotmail.com>
2019-10-15 20:59:11 +0700

    refactor name

--- app/src/main/java/com/android/example/icafe/MenuFragment.kt
+++ app/src/main/java/com/android/example/icafe/MenuFragment.kt
@@ -10,7 +10,7 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class MenyFragment : Fragment() {
+class MenuFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
